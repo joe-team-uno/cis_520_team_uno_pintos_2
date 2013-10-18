1be033242bdf1e8d84c94fa57325b10661d1eb2a
joe-team-uno <jj441@ksu.edu>
2013-10-18 12:22:05 -0500

    working on the sys_open

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -47,8 +47,42 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  typedef int syscall_function(int, int, int);
+
+  struct syscall
+  {
+    size_t arg_cnt;
+    syscall_function *func;
+  };
+
+  static const struct syscall syscall_table[]=
+  {
+	{0, (syscall_function *) sys_halt},
+    {1, (syscall_function *) sys_exit},
+    {1, (syscall_function *) sys_exec},
+	{1, (syscall_function *) sys_wait},
+	{2, (syscall_function *) sys_create},
+	{1, (syscall_function *) sys_remove},
+    {1, (syscall_function *) sys_open}
+  };
+  
+  const struct syscall *sc;
+  unsigned call_nr;
+  int args[3];
+  printf("here1\n");
+  copy_in (&call_nr, f->esp, sizeof call_nr);
+  printf("call num = %d\n", call_nr);
+  if (call_nr >= sizeof syscall_table / sizeof *syscall_table)
+    thread_exit ();
+  sc = syscall_table + call_nr;
+
+  ASSERT (sc->arg_cnt <= sizeof args/ sizeof *args);
+  memset (args, 0, sizeof args);
+  copy_in (args, (uint32_t *) f->esp + 1, sizeof *args * sc->arg_cnt);
+  printf("here2\n");
+  f->eax = sc->func (args[0], args[1], args[2]);
+  
+  
 }
 
 /* Returns true if UADDR is a valid, mapped user address,
